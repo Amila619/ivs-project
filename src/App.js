ae51f1d2eba8c218732b19ac861300e1bfa77d19
Amila619 <amilasandeepa349@gmail.com>
2023-07-09 10:59:07 +0530

    Initial Commit Main UI created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Person from './assets/person_FILL0_wght300_GRAD0_opsz48.svg'
+import Items from './assets/align_items_stretch_FILL0_wght300_GRAD0_opsz48.svg'
+import Categories from './assets/category_FILL0_wght300_GRAD0_opsz48 (1).svg'
+import Suppliers from './assets/group_FILL0_wght300_GRAD0_opsz48.svg'
+import Customers from './assets/assignment_ind_FILL0_wght300_GRAD0_opsz48.svg'
+import Sales from './assets/point_of_sale_FILL0_wght300_GRAD0_opsz48.svg'
+import Menu from './assets/menu_FILL0_wght400_GRAD0_opsz48.svg'
+import Search from './assets/search_FILL0_wght400_GRAD0_opsz48.svg'
+import Bell from './assets/notifications_FILL1_wght400_GRAD0_opsz48.svg'
+import User from './assets/account_circle_FILL0_wght400_GRAD0_opsz48.svg'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Menu} alt='' />
+        <div className='left-icons-header'>
+          <img src={Search} alt='' />
+          <img src={Bell} alt='' />
+          <img src={User} alt='' />
+          <select name='status' className='user-logged'>
+            <option value="Thushara Lakshan">Thushara Lakshan</option>
+            <option value="Amila Sandeepa">Amila Sandeepa</option>
+            {/* <option value="">Thushara Lakshan</option> */}
+          </select>
+        </div>
       </header>
+      <div className='main-container'>
+        <div className='right-container'>
+          <div className='box-icon top'>
+            <img src={Person} alt='' />
+            <span className='inner-box-text'>User</span>
+          </div>
+          <div className='box-icon'>
+            <img src={Items} alt='' />
+            <span className='inner-box-text'>Items</span>
+          </div>
+          <div className='box-icon'>
+            <img src={Categories} alt='' />
+            <span className='inner-box-text'>Categories</span>
+          </div>
+          <div className='box-icon'>
+            <img src={Suppliers} alt='' />
+            <span className='inner-box-text'>Suppliers</span>
+          </div>
+          <div className='box-icon'>
+            <img src={Customers} alt='' />
+            <span className='inner-box-text'>Customers</span>
+          </div>
+          <div className='box-icon down'>
+            <img src={Sales} alt='' />
+            <span className='inner-box-text'>Sales</span>
+          </div>
+        </div>
+        <div className='left-container'></div>
+      </div>
     </div>
   );
 }
